d205a9f6774096b0fe1ca87477c500876b16b3e2
Ian Delahorne <idelahorne@twitch.tv>
2016-07-28 13:28:40 -0700

    First commit of demo go app

---
+++ Makefile
@@ -0,0 +1,10 @@
+all: deb
+
+prep:
+	mkdir -p build/opt/twitch/u2f-honk
+
+demoapp:
+	go build -ldflags "-X main.VERSION=`cat VERSION`" -o ./build/opt/twitch/demoapp/demoapp demoapp.go
+
+deb: prep demoapp
+	fpm -t deb -n demoapp --iteration 1 -v `cat VERSION` --deb-upstart demoapp.conf --after-install deb-post-install.sh -p build -s dir -C build
---
+++ demoapp.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "I'm alright")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there!")
+}
+
+func main() {
+	http.HandleFunc("/healthcheck", healthHandler)
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+	
